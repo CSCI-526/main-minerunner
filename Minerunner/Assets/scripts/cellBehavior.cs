97d979a2bdec2d64af0be120b1f4fc5ca795e18e
Nirupama Mallikarjunaiah <nmallika@usc.edu>
2025-02-26 19:35:33 -0800

    added to cell script

--- Minerunner/Assets/scripts/cellBehavior.cs
+++ Minerunner/Assets/scripts/cellBehavior.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// forward declaration needs to be done to deal with circular dependancy
+
 public class cellBehavior : MonoBehaviour
 {
     private bool hasMine, hasPowerUp, empty, revealed, flagged, isGoal, playerOn;
     private int numMines;
-
+    // public Player player;
+    // public GameMaster gamesmaster;
     // Start is called before the first frame update
     void Start()
     {
         //sets revealed status to false immediately.
         revealed = false;
+        // link the player object to the existing object
+        //player = FindObjectOfType<Player>();
+        // gamemaster = FindObjectOfType<GameMaster>();
+
     }
 
     // Update is called once per frame
@@ -31,7 +39,10 @@ public class cellBehavior : MonoBehaviour
         {
             if (playerOn == true)
             {
-
+                 // if (player.getLives() == 0) {
+                 //    gamemaster.end() } 
+                 // else {
+                 //    player.setLives() }
             }
             hasMine = false;
             //checks in a range if there are other cells containing mines and explodes them based on distance.
@@ -44,7 +55,7 @@ public class cellBehavior : MonoBehaviour
         {
             if (playerOn == true)
             {
-                //calls addPowerup()
+                //calls player.addPowerup()
             }
             //shows powerup if revealed by detonator
         }
@@ -54,7 +65,7 @@ public class cellBehavior : MonoBehaviour
     {
         if (playerOn == true && hasPowerUp == true)
         {
-            //calls addPowerup()
+            //calls player.addPowerup()
         }
     }
 }
