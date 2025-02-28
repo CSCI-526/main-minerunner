133b0a9933e6d8d5146bafd8a15edc5118dd2bc2
theTY2002 <71263782+theTY2002@users.noreply.github.com>
2025-02-27 23:05:34 -0800

    Game Master script changed

--- Minerunner/Assets/scripts/gameMaster.cs
+++ Minerunner/Assets/scripts/gameMaster.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class gameMaster : MonoBehaviour
 {
+    public static int totalMines;
+    private bool goalReached;
+    private bool playerDead;
+
+    public void setMines(int mines)
+    {
+        totalMines = mines;
+    }
+
+    public int getMines()
+    {
+        return totalMines;
+    }
+
+    public bool endGame()
+    {
+        //Maybe split this into 2 cases later
+        if (goalReached || playerDead)
+        {
+            return true;
+        }
+        return false;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalMines = 0;
+        goalReached = false;
+        playerDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Might call getLives from player to set playerDead here
     }
 }
