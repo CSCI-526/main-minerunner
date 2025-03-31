9077be4c53456b58bf9d7d01d8c7d268946e8770
Nirupama Mallikarjunaiah <nmallika@usc.edu>
2025-03-30 23:29:41 -0700

    menu selection using keys
    added the option only for the main menu

---
+++ Minerunner/Assets/scripts/menuController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour
+{
+    public Button[] menuButtons; 
+    private int selectedIndex = 0; 
+
+    void Start()
+    {
+        HighlightButton();
+    }
+
+    void Update()
+    {
+        HandleKeyboardInput();
+    }
+
+    private void HandleKeyboardInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) //UP OR W
+        {
+            selectedIndex = (selectedIndex - 1 + menuButtons.Length) % menuButtons.Length;
+            HighlightButton();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) // UP OR S
+        {
+            selectedIndex = (selectedIndex + 1) % menuButtons.Length;
+            HighlightButton();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) // ENTER OR SPACE
+        {
+            menuButtons[selectedIndex].onClick.Invoke();
+        }
+    }
+
+    private void HighlightButton()
+    {
+        foreach (Button btn in menuButtons)
+        {
+            ColorBlock colors = btn.colors;
+            colors.normalColor = Color.white;
+            btn.colors = colors;
+        }
+
+        ColorBlock selectedColors = menuButtons[selectedIndex].colors;
+        selectedColors.normalColor = Color.yellow; //HIGHLIGHT IN YELLOW FOR NOW CAN CHANGE COLOUR LATER
+        menuButtons[selectedIndex].colors = selectedColors;
+    }
+}
